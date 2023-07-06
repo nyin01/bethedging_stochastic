a8535787a3204e0d413842770a2cf83233b509a8
Maya Weissman <47795173+mweissman97@users.noreply.github.com>
2023-07-06 10:53:20 -0400

    Update p1_meanmat.m

--- matlab_sims/p1_meanmat.m
+++ matlab_sims/p1_meanmat.m
@@ -1,4 +1,7 @@
 function T = p1_meanmat(wa, wb, s, pSpec, N, emodel)
+%function for both the E1P1 and E0P1 models
+%final argument is whether env stoch is on 1 or off 0
+
 gmf = geomean([wa, wb]) + s;
 wc = (5*pSpec - 2*(4*gmf^2 + (9*pSpec^2)/4)^(1/2))/(4*pSpec - 4);
 
@@ -10,17 +13,16 @@ yy0(1) = 1;
 Ta = [Ta yy0];
 Tb = [Tb yy0];
 for i = 1:N
-    x0 = i;
+    x0 = i; %phenotypic distribution is a function of the number of bh individuals in the population
     allc = [0:x0]; %all options for realized bh counts with each phenotype
-    phenodist = binopdf(allc, x0, pSpec);
+    phenodist = binopdf(allc, x0, pSpec); %probability of experiencing every possible realized pS
     r_pS = allc./x0; % all possibilities for realized pS
     
     tmata = [];
     tmatb = [];
-    for j = 1:length(r_pS)
+    for j = 1:length(r_pS) %for every possible realized pS --> what is the fitness of the BH --> what is the transition vector
         pS = r_pS(j);
         
-        %for the p1 model
         %env A
         wbh_a = wc*(1-pS)+wa*(pS);
         wbar_a = (x0*wbh_a+(N-x0)*wa)/N;
@@ -29,7 +31,7 @@ for i = 1:N
         if isnan(mean(y_a))
             y_a = binopdf(x,N,1.0);
         end
-        y = y_a.*phenodist(j);
+        y = y_a.*phenodist(j); %weight transition probability vector by probability of having this realized pS
         tmata = [tmata; y];
         
         % env b
@@ -40,24 +42,24 @@ for i = 1:N
         if isnan(mean(y_b))
             y_b = binopdf(x,N,1.0);
         end
-        yb = y_b.*phenodist(j);
+        yb = y_b.*phenodist(j); %weight transition probability vector by probability of having this realized pS
         tmatb = [tmatb; yb];
         
         
     end
     
-    yya = sum(tmata);
+    yya = sum(tmata); %weighted mean of all transition probabilities
     Ta = [Ta yya'];
     
-    yyb = sum(tmatb);
+    yyb = sum(tmatb); %weighted mean of all transition probabilities
     Tb = [Tb yyb'];
 end
 
-if emodel == 0
+if emodel == 0 %if no env stochasticity, only 2 possible env regimes
     AB = Tb*Ta;
     BA = Ta*Tb;
     T = (AB+BA)./2;
-elseif emodel == 1
+elseif emodel == 1 %if env stochasticity, 4 possible env regimes
     AB = Tb*Ta;
     BA = Ta*Tb;
     AA = Ta*Ta;
