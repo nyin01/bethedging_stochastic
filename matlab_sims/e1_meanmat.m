d463da3c0804e8c75d3557b064d8daad8602610e
Maya Weissman <47795173+mweissman97@users.noreply.github.com>
2023-07-06 10:48:09 -0400

    Update e1_meanmat.m

--- matlab_sims/e1_meanmat.m
+++ matlab_sims/e1_meanmat.m
@@ -1,13 +1,15 @@
 function T = e1_meanmat(wa, wb, s, pS, N)
+    %effective generation matrix for E1P0 model
     g = 1;
     gmf = geomean([wa, wb]) + s;
     wc = (5*pS - 2*(4*gmf^2 + (9*pS^2)/4)^(1/2))/(4*pS - 4); 
     
     wbha = wc*(1-pS)+wa*(pS);
-    Ta = construct_markov(N, wbha, wa);
+    Ta = construct_markov(N, wbha, wa); %env a matrix if pSpec is exactly equal to the expectation
     wbhb = wc*(1-pS)+wb*(pS);
-    Tb = construct_markov(N, wbhb, wb);
-    
+    Tb = construct_markov(N, wbhb, wb); %env b matrix if pSpec is exactly equal to the expectation
+
+    %four possible environmental regimes
     AB = Tb^g*Ta^g;
     BA = Ta^g*Tb^g;
     AA = Ta^g*Ta^g;
